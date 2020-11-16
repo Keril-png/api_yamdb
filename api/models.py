aa0b400b7318328a855d50335b8bbbf5f4b87774
Evlogiy Sutormin <evlogij@gmail.com>
2020-11-16 21:27:34 +0300

    sort imports

--- api/models.py
+++ api/models.py
@@ -1,8 +1,9 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth import get_user_model
-from django.core.validators import MaxValueValidator, MinValueValidator
 from datetime import date
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 
 
 class UserRole(models.TextChoices):
