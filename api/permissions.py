9cda5bf11c600387d17556c336deb4fc249ddd91
Evlogiy Sutormin <evlogij@gmail.com>
2020-11-19 13:44:04 +0300

    remove parentheses

--- api/permissions.py
+++ api/permissions.py
@@ -4,14 +4,14 @@ from rest_framework import permissions
 class IsAdminPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.user.is_staff or
-                (request.user.is_authenticated and request.user.is_admin))
+                request.user.is_authenticated and request.user.is_admin)
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.user.is_staff or
                 request.method in permissions.SAFE_METHODS or
-                (request.user.is_authenticated and request.user.is_admin))
+                request.user.is_authenticated and request.user.is_admin)
 
 
 class IsAuthorOrModeratorOrAdmin(permissions.BasePermission):
